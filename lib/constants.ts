3f525d706be0887e1d52116b0558e70254d2eccc
Jack Humphries <jackphumphries02@gmail.com>
2025-11-10 16:21:24 +0000

    Update GITHUB_URL constant to new URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const EMAIL_ADDRESS = "contact@jackh.sh";
 
-export const GITHUB_URL = "https://github.com/JackHumphries9";
+export const GITHUB_URL = "https://github.com/jackh-sh";
 export const LINKED_IN_URL = "https://www.linkedin.com/in/jack-humphries/";
